e5d83d65eecf7589373593254e8affb6c4702491
SantiAgudo <santiagoagudo52@gmail.com>
2026-03-09 11:03:37 -0500

    Add model training code to train_model.py

---
+++ train_model.py
@@ -0,0 +1,9 @@
+# Model Training
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_data=val_generator,
+    validation_steps=val_generator.samples // val_generator.batch_size,
+    epochs=25
+)
